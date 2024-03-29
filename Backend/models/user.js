307d7b19df69ce382a9297534e8d5303705bffaa
AnshMehta24 <ansh.mehta.y24@gmail.com>
2024-03-29 14:34:17 +0530

    commit

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -12,38 +12,3 @@ const UserDetailSchema = new mongoose.Schema({
 mongoose.model("UserInfo", UserDetailSchema)
 
 
-// const userSchema = new mongoose.Schema({
-//     name:{
-//         type:String,
-//         required:true
-//     },
-//     email:{
-//         type:String,
-//         required: true,
-//         unique: true,
-//     },
-//     password:{
-//         type:String,
-//         required : true,
-//     }
-// }, {
-//     collection : "UserInfo"
-// })
-// module.exports =mongoose.model('User', userSchema)
-// // mongoose.model("UserInfo", userSchema)
-
-// // userSchema.statics.isThisEmailInUse = async function(email){
-// //     if(!email) throw new Error('Invalid Email')
-// //   try {
-// //     const user = await this.findOne({email})
-
-// //     if(user) return false
-
-// //     return true;
-// //   } catch (error) {
-// //     console.log('err', err.message);
-// //     return false;
-// //   }
-// // }
-
-// // module.exports =mongoose.model('User', userSchema)
